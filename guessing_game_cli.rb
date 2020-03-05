6449ab1ee416534caf5c9ce924b59a26926f2743
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 22:08:00 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 def run_guessing_game
   
-      puts "Guess a number between 1 and 6."
+      p "Guess a number between 1 and 6."
       guess = gets.chomp
       computer_number = rand(1..6)
       if guess.downcase == "exit"
