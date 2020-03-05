458149b78cd16a362ed5d8af08caec8fcb62c1e7
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 22:01:43 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def run_guessing_game
-  loop do
+  
       puts "Guess a number between 1 and 6."
       guess = gets.chomp
       computer_number = rand(1..6)
@@ -14,5 +14,4 @@ def run_guessing_game
       else
           puts "Invalid input"
       end
-  end
 end
