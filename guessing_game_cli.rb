37a46737ebffec68346003ce3690c967a3ba5528
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 22:11:24 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,6 +9,6 @@ def run_guessing_game
       elsif guess == computer_number.to_s
           puts "You guessed the correct number!"
       else
-          puts "The computer guessed #{computer_number}."
+          puts "Sorry! The computer guessed #{computer_number}."
       end
 end
