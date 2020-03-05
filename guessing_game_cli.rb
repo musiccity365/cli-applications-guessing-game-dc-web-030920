af2b278c7d2a872202c1f3a668f0d3e0de2e2b4f
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 22:05:14 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,14 +4,11 @@ def run_guessing_game
       puts "Guess a number between 1 and 6."
       guess = gets.chomp
       computer_number = rand(1..6)
-      if guess == "exit"
+      if guess.downcase == "exit"
         puts "Goodbye!"
-        break
       elsif guess.to_i == computer_number
           puts "You guessed the correct number!"
-      elsif guess.to_i != computer_number
-          puts "The computer guessed #{computer_number}."
       else
-          puts "Invalid input"
+          puts "The computer guessed #{computer_number}."
       end
 end
